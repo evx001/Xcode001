4127305c62f2b0cd9aeb1634347038fad35ed795
evx001 <eviexeris@gmail.com>
2014-05-01 01:09:13 -0400

    Initial Commit

---
+++ ChatSend/ChatSend/EVXViewController.h
@@ -0,0 +1,13 @@
+//
+//  EVXViewController.h
+//  ChatSend
+//
+//  Created by evx on 5/1/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EVXViewController : UIViewController
+
+@end
---
+++ ChatSend/ChatSendTests/ChatSendTests.m
@@ -0,0 +1,34 @@
+//
+//  ChatSendTests.m
+//  ChatSendTests
+//
+//  Created by evx on 5/1/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChatSendTests : XCTestCase
+
+@end
+
+@implementation ChatSendTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
