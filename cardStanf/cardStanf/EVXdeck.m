6609a1ce593b5064a5938791ac464f3374d1bda8
evx001 <eviexeris@gmail.com>
2014-05-16 17:02:39 -0400

    mutable array

--- cardStanf/cardStanf/EVXdeck.m
+++ cardStanf/cardStanf/EVXdeck.m
@@ -9,9 +9,25 @@
 #import "EVXdeck.h"
 
 @interface EVXdeck () // because it connects back to header?
-
+@property (strong, nonatomic) NSMutableArray *cards;// strong because we want it to stay in the heap as long as there are pointers for it.
 @end
 
 @implementation EVXdeck
+- (void)addCard:(Card *)card atTop:(BOOL)atTop; {
 
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self.cards addObject:card];
+    }
+}
+
+
+- (void)addCard:(Card *)card;
+{
+    [self addCard:card atTop:NO];
+}
+- (Card *)drawRandomCard;{
+    
+}
 @end
