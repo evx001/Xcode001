ecc4f7037d594c81b72ebfae74af5fa3e4423645
evx001 <eviexeris@gmail.com>
2014-05-16 16:35:49 -0400

    adds associated views but otherwise breaks on Card
    adds associated views but otherwise breaks on Card class

--- stanCard001/stanCard001/EVXViewController.h
+++ stanCard001/stanCard001/EVXViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface EVXViewController : UIViewController
+@property (strong, nonatomic) NSString *contents;
+@property (nonatomic, getter = isChosen) BOOL chosen;
+@property (nonatomic, getter = isMatched) BOOL matched;
+//-(int)match:(Card *)card;
 
 @end
