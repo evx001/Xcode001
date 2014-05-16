f386a82ca5db53500afe577b16a0952e2627ce31
evx001 <eviexeris@gmail.com>
2014-05-16 16:33:31 -0400

    Deck

---
+++ cardStanf/cardStanf/EVXdeck.h
@@ -0,0 +1,13 @@
+//
+//  EVXdeck.h
+//  cardStanf
+//
+//  Created by evx on 5/16/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EVXdeck : NSObject
+
+@end
---
+++ cardStanf/cardStanf/EVXdeck.m
@@ -0,0 +1,17 @@
+//
+//  EVXdeck.m
+//  cardStanf
+//
+//  Created by evx on 5/16/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+#import "EVXdeck.h"
+
+@interface EVXdeck () // because it connects back to header?
+
+@end
+
+@implementation EVXdeck
+
+@end
