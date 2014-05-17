2e2c210eb4b0ca8039e41f3519b3b656f93dfdcf
evx001 <eviexeris@gmail.com>
2014-05-17 16:12:16 -0400

    PlayingCard Support file

---
+++ cardStanf/cardStanf/PlayingCard.h
@@ -0,0 +1,16 @@
+//
+//  PlayingCard.h
+//  cardStanf
+//
+//  Created by evx on 5/17/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+#ifndef cardStanf_PlayingCard_h
+#define cardStanf_PlayingCard_h
+
+
+
+#endif
+@interface PlayingCard : Card
+
