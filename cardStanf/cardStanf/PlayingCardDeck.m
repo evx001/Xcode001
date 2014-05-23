259299777f8c212f667c0075b4f916e6770dd9e2
evx001 <eviexeris@gmail.com>
2014-05-23 18:29:57 -0400

    commenting out method init // too many warnings

--- cardStanf/cardStanf/PlayingCardDeck.m
+++ cardStanf/cardStanf/PlayingCardDeck.m
@@ -9,19 +9,19 @@
 #import "PlayingCardDeck.h"
 #import "PlayingCard.h"
 @implementation PlayingCardDeck
-- (instancetype)init
-{
-    // wraps an alloc
-    self = [super init]; // weird but ....
-    if (self) {
-        for (NSString *suit in [PlayingCard validSuits]){
-            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
-                PlayingCard *card = [[PlayingCard alloc]init];
-                card.rank = rank;
-                card.rank = suit;
-            }
-        }
-    }
-    return self;
-}
+//- (instancetype)init
+//{
+//    // wraps an alloc
+//    self = [super init]; // weird but ....
+//    if (self) {
+//        for (NSString *suit in [PlayingCard validSuits]){
+//            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+//                PlayingCard *card = [[PlayingCard alloc]init];
+//                card.rank = rank;
+//                card.rank = suit;
+//            }
+//        }
+//    }
+//    return self;
+//}
 @end
